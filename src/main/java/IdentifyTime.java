6924c92a5d3f77750b5fd90cb06df0252497193a
AlexisCMD <shuripaa@gmail.com>
2018-04-12 04:44:32 +0300

    control point cleaning IdentifyTime

--- src/main/java/IdentifyTime.java
+++ src/main/java/IdentifyTime.java
@@ -3,10 +3,10 @@ import java.util.TimeZone;
 
 public class IdentifyTime {
 
-    private Calendar c;
     private TimeZone tz;
     private String stz;
     private String town;
+    private Calendar c;
 
     private Integer hour;
     private Integer minute;
@@ -17,54 +17,35 @@ public class IdentifyTime {
     }
 
     public IdentifyTime(String townName) {
-        this.town = townName;
-        TimeZone tz = isIdentifyTimeZone(townName);
-        //Определение тайм-зоны
-//        if (tz.equals(TimeZone.getDefault()))
-//            System.out.println("Города нет в списке");
-//        else {
-//            System.out.println("Город есть в списке");
-//        }
-        //Получение времени
-        Calendar c = Calendar.getInstance(tz);
-//        String time = c.get(Calendar.HOUR_OF_DAY) + ":" + c.get(Calendar.MINUTE) + ":" + c.get(Calendar.SECOND);
-//        System.out.println(time);
-//        stz = "";
-        //Вывод сообщения
-        this.hour = c.get(Calendar.HOUR_OF_DAY);
-        this.minute = c.get(Calendar.MINUTE);
+        town = townName;
+        tz = isIdentifyTimeZone(townName);
+        c = Calendar.getInstance(tz);
+        hour = c.get(Calendar.HOUR_OF_DAY);
+        minute = c.get(Calendar.MINUTE);
+        printHello();
     }
 
 
     public IdentifyTime(String townName, String timeZone) {
-        this.town = townName;
-        this.stz = timeZone;
+        town = townName;
+        stz = timeZone;
     }
 
     private void printHello() {
-
+        System.out.println("Hello " + town);
     }
 
-    public TimeZone isIdentifyTimeZone(String town){
-        Boolean isIdentify = false;
+    private TimeZone isIdentifyTimeZone(String town){
+
         String[] tzs = TimeZone.getAvailableIDs();
         String ttown = "";
         TimeZone gmt = TimeZone.getDefault();
-        Calendar c;
-//        String time;
 
         for (String tz : tzs){
-//             System.out.println(tzs[i].lastIndexOf("/")+tzs[i]);
             ttown = tz.substring(tz.lastIndexOf("/") + 1, tz.length());
             if (ttown.equals(town)){
                 gmt = TimeZone.getTimeZone(ttown);
             }
-//            System.out.print(ttown + " / ");
-//            TimeZone gmt = TimeZone.getTimeZone(ttown);
-//            System.out.print(gmt.getRawOffset()/60/60/1000 + " / ");
-//            c = Calendar.getInstance(TimeZone.getTimeZone(ttown));
-//            time = c.get(Calendar.HOUR_OF_DAY) + ":" + c.get(Calendar.MINUTE) + ":" + c.get(Calendar.SECOND);
-//            System.out.println(time);
         }
 
         return gmt;
