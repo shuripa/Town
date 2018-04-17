bd84532274c756e7595bbc58712f0bc9c5acaf98
Alexis <shuripaa@gmail.com>
2018-04-17 12:37:34 +0300

    correction name.

--- src/main/java/Town.java
+++ src/main/java/Town.java
@@ -5,8 +5,10 @@ public class Town {
     //Changed swithc-case
     public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException, UnsupportedEncodingException {
         String[] clName = {"GetGreetingWhisoutArgs", "GetGreetingWhisOneArg", "GetGreetingWhisTwoArgs"};
-        int cnt = (args.length >2)? 2: args.length;
-         
+        int cnt = (args.length >2) ? 2: args.length;
+        //Correction Name
+        if (args.length > 1)  args[0] = args[0].replace(' ', '_');
+        
         GetGreeting grt = (GetGreeting) Class.forName(clName[cnt]).getConstructor().newInstance();
         String result = grt.getGreeting(args);
         System.out.println(result); 
